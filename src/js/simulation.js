d215cb29bc538aa2b8038e60ce66db5faac5d178
wildone <max.barrass@gmail.com>
2025-02-12 08:46:58 +1100

    fix null check.

--- src/js/simulation.js
+++ src/js/simulation.js
@@ -23,6 +23,9 @@ window.Widgets.Simulation = {};
         if (typeof json != 'string') {
              json = JSON.stringify(json, undefined, 2);
         }
+        if (!json || json === 'undefined') {
+            return "";
+        }
         json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
         return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
             var cls = 'number';
